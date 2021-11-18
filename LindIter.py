82d7b2569a306de5ce3432bee8d52547d2976ea7
dcb011235 <87423855+dcb011235@users.noreply.github.com>
2021-11-18 14:38:30 +0100

    Update LindIter.py

--- LindIter.py
+++ LindIter.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 13 16:20:24 2021
-
-@author: dylan
-"""
-
 def LindIter(System, N):
+    # global System
+    # print(System)
+    # print(N)
     #set empty value
     LindenmayerString = ""
     #Establish string Tranfrom dictionary with rules for letters
-    systemDictionary = {"S":"SLSRSLS","A":"BRARB","B":"ALBLA"}
+    systemDictionary = {"S":"SLSRSLS","A":"BRARB","B":"ALBLA","D":"DLDDRDL"}
     #creates translation table
     rulesTable = LindenmayerString.maketrans(systemDictionary)
     #Initiates Koch sequence
@@ -25,11 +20,22 @@ def LindIter(System, N):
         LindenmayerString = "A"
         for i in range(N):
             LindenmayerString = LindenmayerString.translate(rulesTable)
+    # print(System)
+    # print(N)
 
-                       
-    return LindenmayerString
-
-
-
-
+    lindList = LindenmayerString
+    print(lindList)  
+#def turtlegraph(LindenmayerString)                     
+    if System == "Koch":
+        kochDic = {"S":f"1/3**{N} ","L":"math.pi/3 ","R":"-2*math.pi/3 "}
+        kochTable = lindList.maketrans(kochDic)
+        turtleCommands = lindList.translate(kochTable)
+    elif System == "Sierpinski":
+        sierDic = {"A":f"1/2**{N} ","B":f"1/2**{N} ","L":"math.pi/3 ","R":"-math.pi/3 "}
+        sierTable = lindList.maketrans(sierDic)
+        turtleCommands = lindList.translate(sierTable)
+    # turtleCommands = turtleCommands.split(",")
     
+    turtleCommands = turtleCommands.rstrip()
+    turtleCommands = turtleCommands.split(" ")               
+    return turtleCommands
